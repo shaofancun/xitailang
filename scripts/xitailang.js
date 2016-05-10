a5b735651bb98eb94ef7f09331cb8cb54c25fdd9
shaofancun <185952315@qq.com>
2016-05-10 20:01:18 +0800

    1

--- scripts/xitailang.js
+++ scripts/xitailang.js
@@ -9,21 +9,26 @@ if($('#banner').length>0){
 	$('.prod_show_list').cxScroll();
 }
 //tab切换
-$(".about_left a").click(function(){
-	var tabId=$(this).data("toggle");
-	$(".about_left a").removeClass('active');
+var tab=$(".about_left a");		//所有的tab a标签
+tab.click(function(){
+	var tabId=$(this).data("toggle");		//获取点击的ID
+	tab.removeClass('active');
 	$(this).addClass('active');
 	$(".about_right_box").fadeOut(300);
 	$('#'+tabId+'').fadeIn(300);
 })
+
 //显示隐藏下载框
-$("#downShow").click(function(){
-	$(this).parent().addClass("down");
-	$(".mark").fadeIn(300);
-	$(".down_box").fadeIn(300);
+var mark=$(".mark"),		//遮罩层
+	downBtn=$("#downShow"),		//下载按钮
+	downBox=$(".down_box");		//下载框
+downBtn.click(function(){
+	downBtn.addClass("down");
+	mark.fadeIn(300);
+	downBox.fadeIn(300);	
 })
-$(".mark").click(function(){
-	$(".mark").fadeOut(300);
-	$(".down_box").fadeOut(300);
-	$("#downShow").removeClass("down");
+mark.click(function(){
+	mark.fadeOut(300);
+	downBox.fadeOut(300);
+	downBtn.removeClass("down");
 })
